073e814332f02f631821d3a52c26d7ee641fe911
digitex3d <digitex3d@gmail.com>
2017-01-13 21:24:01 +0100

    Added new marker function

--- app/src/main/java/tpdev/megaphone/MapsActivity.java
+++ app/src/main/java/tpdev/megaphone/MapsActivity.java
@@ -90,40 +90,27 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             childUpdates.put("/messages/" + key, messageValues);
             databaseRef.updateChildren(childUpdates);
 
+            add_message_marker(email, msg);
+
         }
 
 
 
     }
 
-
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+    public void add_message_marker(String user, String msg){
         // Add a marker in Sydney and move the camera
-        listMarker = new ArrayList<>();
         LatLng jussieu = new LatLng(48.862725, 2.287592000000018);
-        MarkerOptions juss = new MarkerOptions().position(jussieu).title("lat : "+jussieu.latitude+", long : "+jussieu.longitude).snippet("Jussieu");
-        mMap.addMarker(juss);
+
+        MarkerOptions shout_marker = new MarkerOptions().position(jussieu)
+                .title(user)
+                .snippet(msg);
+
+        mMap.addMarker(shout_marker);
         //float zoom = 16;
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(jussieu, 7));
 
-        LatLng point1 = new LatLng(50, 3);
-        MarkerOptions p1 = new MarkerOptions().position(point1).title("lat : "+point1.latitude+", long : "+point1.longitude).snippet("Point1");
-        mMap.addMarker(p1);
-
-        listMarker.add(juss);
-        listMarker.add(p1);
+        listMarker.add(shout_marker);
 
 
         mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
@@ -144,6 +131,26 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 mMap.addMarker(newPoint);
             }
         });
+
+    }
+
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        listMarker = new ArrayList<>();
 
     }
 
